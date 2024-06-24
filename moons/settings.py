8c7da76d69784e0ab1309b2c42d7e7e90b95c798
Jair Rafael García Cortés <jairrafaelgarciacortes@Jairs-MacBook-Pro.local>
2024-06-23 19:33:13 -0600

    Configured STATIC_ROOT for Heroku deployment

--- moons/settings.py
+++ moons/settings.py
@@ -15,7 +15,7 @@ import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -139,6 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Adjust the directory as per your project structure
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
