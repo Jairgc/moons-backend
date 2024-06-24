65ec9451d332f5be2ddc2a0af7f69e34b34b5a05
Jair Rafael García Cortés <jairrafaelgarciacortes@Jairs-MacBook-Pro.local>
2024-06-23 21:00:50 -0600

    Collecting static files

--- moons/settings.py
+++ moons/settings.py
@@ -139,9 +139,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-STATICFILES_FINDERS = [
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # Adjust as per your project structure
 ]
 
 
